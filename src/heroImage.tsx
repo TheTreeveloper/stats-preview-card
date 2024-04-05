f76ed845ecfd410c95518c493250d269aed557b5
ioddgamers <toluloperbrts@gmail.com>
2024-04-05 17:25:21 +0100

    Created and modified hero-image

---
+++ src/heroImage.tsx
@@ -0,0 +1,11 @@
+export const MobileHeroImage = () => {
+  return (
+    <div className="overlay">
+      <img
+        src="./public/images/image-header-mobile.jpg"
+        alt="Female computer scientists"
+        className="hero-mobile"
+      />
+    </div>
+  );
+};
