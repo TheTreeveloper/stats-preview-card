ef53d6f692a3aa4f28188c10207bf3cb11a8520a
ioddgamers <toluloperbrts@gmail.com>
2024-04-05 12:10:34 +0100

    created TextContent, added text content elements

---
+++ src/textContent.tsx
@@ -0,0 +1,32 @@
+export const TextContent = () => {
+  return (
+    <>
+      <div className="content-container">
+        <h1>
+          Get <span>insights</span> that help your business grow.
+        </h1>
+        <p className="description">
+          Discover the benefits of data analytics and make better decisions
+          regarding revenue, customer experience, and overall efficiency.
+        </p>
+
+        <div className="stats">
+          <div className="sub-stat">
+            <h3>10k+</h3>
+            <p>companies</p>
+          </div>
+
+          <div className="sub-stat">
+            <h3>314</h3>
+            <p>templates</p>
+          </div>
+
+          <div className="sub-stat">
+            <h3>12M+</h3>
+            <p>queries</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
