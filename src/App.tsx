fa7ada601b7c262e6b5aae7542e15ee0a8c52383
ioddgamers <toluloperbrts@gmail.com>
2024-04-07 15:13:37 +0100

    removed react from import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { TextContent } from "./textContent";
 import { MobileHeroImage } from "./heroImage";
 import { Attribution } from "./attribution";
