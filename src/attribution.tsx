93e6c9aaec6dfc459676c710ee5159be6bd4f6a0
ioddgamers <toluloperbrts@gmail.com>
2024-04-07 15:08:48 +0100

    created and added attribution

---
+++ src/attribution.tsx
@@ -0,0 +1,15 @@
+export const Attribution = () => {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io/profile/ioddgamers"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://github.com/TheTreeveloper">Adegbamigbe Tolulope</a>.
+    </div>
+  );
+};
