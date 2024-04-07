7fb8e3589aff76aeaaac69b9d3e21ee1f194a86e
ioddgamers <toluloperbrts@gmail.com>
2024-04-07 15:08:17 +0100

    added function to handle screen responsive depending on the screenwidth, imported Desktop hero image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
+import React, { useState, useEffect } from "react";
 import { TextContent } from "./textContent";
 import { MobileHeroImage } from "./heroImage";
+import { Attribution } from "./attribution";
+import { DesktopHeroImage } from "./heroImage";
 
 function App() {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsDesktop(window.innerWidth >= 1200);
+    };
+    checkScreenWidth();
+    window.addEventListener("resize", checkScreenWidth);
+
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
+
   return (
     <>
-      <div className="container">
-        <MobileHeroImage />
-        <TextContent />
+      <div className="App">
+        <div className="container">
+          {isDesktop && <DesktopHeroImage />}
+          {!isDesktop && <MobileHeroImage />}
+          <TextContent />
+        </div>
+        <Attribution />
       </div>
     </>
   );
